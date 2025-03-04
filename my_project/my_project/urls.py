c0f5baa96a8c9372998f3cc22685ac176113ada8
Duy Lam <duylam468213@gmail.com>
2025-03-04 11:10:12 +0700

    update crud danhsachphat

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nguoidung/' , include('apps.nguoidung.urls'))
+    path('nguoidung/' , include('apps.nguoidung.urls')),
+    path('danhsachphat/' , include('apps.danhsachphat.urls'))
 ]
