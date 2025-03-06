75751fb0e91d388beca1ba4f52f4c0d882830671
Hồ Hưng Lộc <141534402+liam582004@users.noreply.github.com>
2025-03-06 11:04:10 +0700

    Update region s3

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -78,7 +78,7 @@ INSTALLED_APPS = [
 AWS_ACCESS_KEY_ID = 'admin'
 AWS_SECRET_ACCESS_KEY = 'w$;8H?JFfwkAg8+'
 AWS_STORAGE_BUCKET_NAME = 'spotifycloud'
-AWS_S3_REGION_NAME = 'Asia Pacific (Sydney) ap-southeast-2'
+AWS_S3_REGION_NAME = 'ap-southeast-2'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
