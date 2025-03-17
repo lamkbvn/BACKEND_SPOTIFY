6d991a18e5934189e96912627b480af5e8e62652
LocNguyenSGU <lockbkbang@gmail.com>
2025-03-17 10:50:18 +0700

    feat: thanh toan paypal

--- my_project/payment/urls.py
+++ my_project/payment/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import create_zalopay_order, zalopay_callback
+from .views import create_zalopay_order, zalopay_callback, create_paypal_order, capture_paypal_order
 
 urlpatterns = [
     path("zalopay/create/", create_zalopay_order, name="create_zalopay_order"),
     path("zalopay/callback/", zalopay_callback, name="zalopay_callback"),
+    path("paypal/create/", create_paypal_order, name="create_paypal_order"),
+    path("paypal/capture/", capture_paypal_order, name="capture_paypal_order"),
 ]
